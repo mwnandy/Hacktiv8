f4ead8df33b30a66c9bbba17da59fb7e3cca9674
mwnandy <44156845+mwnandy@users.noreply.github.com>
2018-10-23 19:19:58 +0700

    Add files via upload

---
+++ Week 2/E6Perulangan.js
@@ -0,0 +1,35 @@
+/* Looping */
+
+// 1. Melakukan Looping Menggunakan While
+var flag = 2;
+var deret = 2;
+var flag2 = 20;
+
+console.log('LOOPING PERTAMA');
+while(flag <= 20) {
+  console.log(flag + ' - I love coding');
+  flag +=deret;
+}
+
+console.log('LOOPING KEDUA');
+while(flag2 >= 2) {
+  console.log(flag2 + ' - I will become fullstack developer');
+  flag2 -=deret;
+}
+
+================================================================================
+
+// 2. Melakukan Looping Menggunakan For
+
+console.log('LOOPING PERTAMA')
+for(var deret = 1; deret <= 20; deret ++) {
+  console.log(deret + ' - I Love Coding ');
+}
+
+console.log('LOOPING KE DUA')
+for(var deret = 20; deret >= 1; deret --) {
+  console.log(deret + ' - I will become fullstack developer ');
+}
+
+================================================================================
+
