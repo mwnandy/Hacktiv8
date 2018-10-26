e34ad9de2f32a9b450d65aadbbe62edc7fdeda00
mwnandy <44156845+mwnandy@users.noreply.github.com>
2018-10-26 14:25:55 +0700

    Update E5String.js

--- Week 2/E5String.js
+++ Week 2/E5String.js
@@ -10,7 +10,7 @@ var fourth = 'and';
 var fifth = 'I';
 var sixth = 'love';
 var seventh = 'it!';
-console.log(word.concat(' ' + second).concat(' ' + third).concat(' ' + fourth).concat(' ' + fifth).concat(' ' + sixth).concat(' ' + seventh));
+console.log(word+ ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh);
 
 ===========================================================================================================================
 
