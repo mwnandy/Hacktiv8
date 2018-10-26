a221d040e3de94d62d875bff50d7c04eb5c6e408
mwnandy <44156845+mwnandy@users.noreply.github.com>
2018-10-26 11:15:38 +0700

    Add files via upload

---
+++ Week 2/IfElse.js
@@ -0,0 +1,24 @@
+var nama = 'Nandy';
+var peran = 'Ksatria';
+
+if(nama === ''){
+  console.log('Nama harus di isi!');
+}
+  else if(peran === ''){
+    console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!')
+  }
+  else if (peran === 'Ksatria'){
+    console.log('Selamat datang di Dunia Proxytia, ' + nama)
+    console.log('Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!')
+  }
+  else if(peran === 'Tabib'){
+    console.log('Selamat datang di Dunia Proxytia, ' + nama)
+    console.log('Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.')
+  }
+  else if(peran === 'Penyihir'){
+    console.log('Selamat datang di Dunia Proxytia, ' + nama)
+    console.log('Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu')
+  }
+  else {
+    console.log('Halo kamu salah memilih peran silahkan masukkan lagi')
+  }
