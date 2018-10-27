f077ac84815fac91867f21243c2d92501c993726
mwnandy <44156845+mwnandy@users.noreply.github.com>
2018-10-27 13:12:54 +0700

    Update E9Function.js

--- Week 2/E9Function.js
+++ Week 2/E9Function.js
@@ -2,9 +2,9 @@
 // Tugas 1
 // Buatlah sebuah fungsi bernama shoutOut(), yang mengembalikan nilai berupa "Halo Function!", yang kemudian akan ditampilkan di console.
 function shoutOut(){
-  console.log("Halo Function")
+    return 'Halo Function!';
 }
-shoutOut();
+console.log(shoutOut())
 
 ===========================================================================================
 
