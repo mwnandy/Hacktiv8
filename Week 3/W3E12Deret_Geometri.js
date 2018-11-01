21b25b86ccbb89f30233ab17f1fb381aed3689c9
mwnandy <44156845+mwnandy@users.noreply.github.com>
2018-11-01 16:42:00 +0700

    Add files via upload

---
+++ Week 3/W3E12Deret_Geometri.js
@@ -0,0 +1,23 @@
+function tentukanDeretGeometri(arr) {
+  // you can only write your code here!
+  var hitung = '';
+  var pembanding = '';
+  for(var i=arr.length-1; i>=0; i--){
+    hitung = arr[i] / arr[i-1]
+    pembanding = arr[i-1] / arr[i-2]
+    if (hitung === pembanding){
+      return true;
+    }
+      else {
+        return false;
+      }
+  }
+  //return hitung;
+}
+
+// TEST CASES
+console.log(tentukanDeretGeometri([1, 3, 9, 27, 81])); // true
+console.log(tentukanDeretGeometri([2, 4, 8, 16, 32])); // true
+console.log(tentukanDeretGeometri([2, 4, 6, 8])); // false
+console.log(tentukanDeretGeometri([2, 6, 18, 54])); // true
+console.log(tentukanDeretGeometri([1, 2, 3, 4, 7, 9])); // false
